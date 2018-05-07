0605f075b0f0ed6423e2ab372aa9af1c43409b4b
St John Giddy <stjohngiddy@gmail.com>
2018-05-07 11:10:40 +0200

    fix(schema): text should be optional

--- src/Endpoint.js
+++ src/Endpoint.js
@@ -7,19 +7,19 @@ class Endpoint {
     this.uri = api
 
     this.mutate = `
-      mutation addEvent($text: String!, $timestamp: String!, $stocks: [String], $hazards: [String], $event_type: String, $source: String) {
-        event(text: $text, timestamp: $timestamp, stocks: $stocks, hazards: $hazards, event_type: $event_type, source: $source) {
+      mutation addEvent($text: String, $timestamp: String!, $stocks: [String], $hazards: [String], $event_type: String, $source: String, $locality: String) {
+        event(text: $text, timestamp: $timestamp, stocks: $stocks, hazards: $hazards, event_type: $event_type, source: $source, locality: $locality) {
           key
         }
       }
     `
   }
 
-  post(value) {
+  post(value, check = true) {
 
     const { timestamp } = value
 
-    if(this.check(value)) {
+    if(!check || this.check(value)) {
       return request(this.uri, this.mutate, value).then(data => console.log(data))
     }
     return new Promise(() => false)
@@ -30,7 +30,7 @@ class Endpoint {
     if(hazards === undefined || stocks === undefined) {
       return false
     }
-    return hazards.length > 0 && stocks.length > 0
+    return hazards.length > 0 || stocks.length > 0
   }
 }
 
