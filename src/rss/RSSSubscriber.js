686653947b8ca358a5a4b01b33a34dcf16575e5b
St John Giddy <stjohngiddy@gmail.com>
2018-05-07 11:13:50 +0200

    feat(rss): match feed item description and post to endpoint

--- src/rss/RSSSubscriber.js
+++ src/rss/RSSSubscriber.js
@@ -5,11 +5,13 @@
  */
 const FeedParser = require('feedparser');
 const request = require('request')
+const fetch = require('node-fetch')
 
 /**
  * module imports
  */
-const Module =  require('../Module')
+const Module = require('../Module')
+const Keyword = require('../hooks/Keyword')
 
 /**
  * @class RSSSubscriber
@@ -17,12 +19,31 @@ const Module =  require('../Module')
  * @param log
  */
 class RSSSubscriber extends Module {
-  constructor({ urls, log }) {
+  constructor({
+    urls,
+    log
+  }) {
     super('rss')
     this.urls = urls
     this.log = log ? (txt) => log.debug(txt) : console.log
     const logger = this.log
-    this.feedparser = new FeedParser({addmeta: false})
+    this.feedparser = new FeedParser({
+      addmeta: false
+    })
+
+  }
+
+  init({
+    locationKwords,
+    hooks: {
+      stocks,
+      hazards
+    }
+  }) {
+
+    const hazardHandler = new Keyword(hazards.keywords)
+    const stockHandler = new Keyword(stocks.keywords)
+    const locationHandler = new Keyword(locationKwords)
 
     this.feedparser.on('readable', function () {
       // This is where the action is!
@@ -30,15 +51,28 @@ class RSSSubscriber extends Module {
       var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
       var item;
       while (item = stream.read()) {
-        //logger(JSON.stringify(item, null, 2));
+        if (!item) {
+          return
+        }
+        //console.log(item)
+        const {
+          link, description
+        } = item
+
+        const hazMatchs = hazardHandler.match(description)
+        const stockMatchs = stockHandler.match(description)
+        const localityMatchs = locationHandler.match(description)
+
+
+        console.log([hazMatchs, stockMatchs, localityMatchs])
       }
     })
-
   }
+
 
   start() {
     this.log(`RSS fetching feeds from ${this.urls.join(', ')}`)
-    if(this.urls instanceof Array) {
+    if (this.urls instanceof Array) {
       this.urls.map(url => {
         const req = request(url)
         req.on('response', (res, requ) => {
@@ -46,15 +80,14 @@ class RSSSubscriber extends Module {
 
           if (res.statusCode !== 200) {
             this.emit('error', new Error('Bad status code'))
-          }
-          else {
+          } else {
             stream.pipe(this.feedparser)
           }
         })
       })
     }
   }
-  
+
 }
 
 module.exports = RSSSubscriber
